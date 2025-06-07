eea98a9c8e9b555b53b3c5cf1548d3ee308fc7d1
Kitti.Bank <kittitach.pro@gmail.com>
2025-06-07 22:30:37 +0700

    create Routes, verifyOTPEmail

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import userModel from '../models/model';
+import userModel from '../models/model.js';
+import transporter from '../config/nodemailer.js';
 
 export const register = async (req, res) => {
     const { name, email, password } = req.body;
@@ -20,14 +21,28 @@ export const register = async (req, res) => {
         const user = new userModel({ name, email, password: hashedPassword });
         await user.save();
 
+        //Create token
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '3d' });
-        res.cokie('token', token, {
+        res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ?
                 'none' : 'strict',
             maxAge: 3 * 24 * 60 * 60 * 1000
         });
+
+        //Sending welcome Email
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: email,
+            subject: 'ยินดีต้อนรับ คุณลงทะเบียนสำเร็จแล้ว 🎉',
+            html: `<html>
+                            <body style="font-family: sans-serif; line-height: 1.6; color: #333;">
+                            <h2>สวัสดีคุณ ${name},</h2>
+                            <p>ตอนนี้คุณสามารถเข้าสู่ระบบและเริ่มใช้งานระบบของเราได้ทันที</p></body></html>`
+        }
+
+        await transporter.sendMail(mailOptions);
 
         return res.json({ success: true });
 
@@ -45,7 +60,7 @@ export const login = async (req, res) => {
     }
 
     try {
-        //Code
+        //Check user for login
         const user = await userModel.findOne({ email });
 
         if (!user) {
@@ -58,19 +73,103 @@ export const login = async (req, res) => {
             return res.json({ success: false, message: "Invalid password" })
         }
 
+        //Create token
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '3d' });
-        res.cokie('token', token, {
+        res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ?
                 'none' : 'strict',
             maxAge: 3 * 24 * 60 * 60 * 1000
         });
-        
+
         return res.json({ success: true });
 
 
     } catch (err) {
         return res.json({ success: false, message: err.message })
+    }
+}
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ?
+                'none' : 'strict',
+        })
+
+        return res.json({ success: true, message: "Logged Out" })
+
+    } catch (err) {
+        return res.json({ success: false, message: err.message })
+    }
+}
+
+//Send Verifitaion OTP to the User's Email
+export const sendVerifyOtp = async (req, res) => {
+    try {
+        const { userId } = req.body;
+        const user = await userModel.findById(userId);
+
+        if (user.isAccountVerified) {
+            return res.json({ success: false, message: "Account already verified" })
+        }
+
+        //Create OTP number 6 digits
+        const otp = String(Math.floor(100000 + Math.random() * 900000));
+
+        user.verifyOtp = otp;
+        user.verifyOtpExpireAt = Date.now() + 24 * 60 * 60 * 1000
+
+        await user.save();
+
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: user.email,
+            subject: 'การสมัครสมาชิก - กรุณายืนยันรหัส OTP',
+            text: `รหัส OTP ของคุณคือ ${otp} โปรดนำรหัสนี้ยืนยันบนระบบสมัครสมาชิก`
+        }
+        await transporter.sendMail(mailOptions);
+
+        return res.json({ success: true, message: 'ระบบได้ส่งรหัสยืนยัน OTP ไปยัง Email ของคุณแล้ว' })
+
+    } catch (err) {
+        return res.json({ success: false, message: err.message })
+    }
+}
+
+export const verifyEmail = async (req, res) => {
+    const { userId, otp } = req.body;
+
+    if (!userId || !otp) {
+        return res.json({ success: false, message: 'Missing details' });
+    }
+
+    try {
+        const user = await userModel.findById(userId);
+
+        if (!user) {
+            return res.json({ success: false, message: 'User not found' });
+        }
+
+        if (user.verifyOtp === '' || user.verifyOtp !== otp) {
+            return res.json({ success: false, message: 'Invalid OTP' });
+        }
+
+        if (user.verifyOtpExpireAt < Date.now()) {
+            return res.json({ success: false, message: 'OTP Expired' });
+        }
+
+        user.isAccountVerified = true;
+        user.verifyOtp = '';
+        user.verifyOtpExpireAt = 0;
+
+        await user.save();
+        return res.json({ success: true, message: 'Email verified successfully' })
+
+    } catch (err) {
+        return res.json({ success: false, message: err.message });
     }
 }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
+import authRouther from "./routes/authRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
 
+// API Endpoints
 app.get('/', (req, res) => res.send("API Working"));
+app.use('/api/auth', authRouther);
 
 app.listen(port, () => console.log(`Server started on PORT: ${port}`));
