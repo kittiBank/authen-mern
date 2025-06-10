a9f11742849b03c97a6bf9b0035b4c68ea3f4bd2
Kitti.Bank <kittitach.pro@gmail.com>
2025-06-10 23:33:17 +0700

    feat/add login navigate, add Context

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -12,7 +12,8 @@ const userAuth = async (req, res, next) => {
         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
 
         if (tokenDecode.id) {
-            req.body.userId = tokenDecode.id
+            //req.body.userId = tokenDecode.id
+            req.userId = tokenDecode.id
         } else {
             return res.json({ success: false, message: 'Not Authorized. Login Again' })
         }
